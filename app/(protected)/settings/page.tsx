3c2cd820dd26da5ea643e84669941a2924d64cf0
Raghav <raghavbhandari082@gmail.com>
2024-03-16 13:34:07 +0530

    login authentication done and redirected to settings page

---
+++ app/(protected)/settings/page.tsx
@@ -0,0 +1,24 @@
+import { auth, signOut } from "@/auth"
+// import { signOut } from "next-auth/react";
+
+const SettingsPage = async () => {
+
+    const session = await auth()
+
+    return (
+        <div>
+            { JSON.stringify(session) }
+            <form action={async () => {
+                "use server";
+
+                await signOut()
+            }}>
+                <button type="submit">
+                    Sign Out
+                </button>
+            </form>
+        </div>
+    )
+}
+
+export default SettingsPage
